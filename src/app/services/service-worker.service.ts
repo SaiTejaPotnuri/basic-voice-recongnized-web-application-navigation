b07c02cf11aaa13047636f7a1f394d4a4554710e
SaiTejaPotnuri <saiteja.potnuri@imaginnovate.com>
2024-12-11 15:09:28 +0530

    testing service worker

--- src/app/services/service-worker.service.ts
+++ src/app/services/service-worker.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { SwUpdate, VersionEvent } from '@angular/service-worker';
 import { interval } from 'rxjs';
 
